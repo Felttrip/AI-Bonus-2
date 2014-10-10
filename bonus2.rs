b3692840901aa421fb7e05b850e3826b5f5ac777
Eric-Gaudiello <egaudiello@hotmail.com>
2014-10-09 19:38:12 -0500

    added valid move logic

--- bonus2.rs
+++ bonus2.rs
@@ -1,16 +1,57 @@
 //libs
-use std::collections
+use std::collections;
+use std::vec;
+use std::rand;
+use std::io;
 
-struct game{
-	tiles: [int]
+static size: uint = 3;
+
+
+struct gameState{
+	board : Vec<Vec<uint>>,
+	moves_made : uint,
+	zero_pos : (uint, uint)
+}
+
+fn main() {
+	
+	let mut game : gameState;
+	game.zero_pos = (2u,2u);
+	game = generatePuzzle(game);
 
 }
-fn main() {
-	let hi = "hi";
-	let mut count = 0i;
 
-	while count < 10 {
-	    println!("count is {}", count);
-	    count += 1;
+fn generatePuzzle(game : gameState) -> gameState{
+
+	let mut board : Vec<Vec<uint>> = vec![vec![1,2,3], vec![4,5,6], vec![7,8,0]];
+	let mut num_rand_moves = (rand::random::<uint>() % 500u) + 1u;
+
+	validMove(game.zero_pos);
+
+	return board;
+}
+
+/* If there are no valid moves, -1 is returned */
+fn validMove( zero_pos : (uint, uint) ) -> Vec<(uint, uint)>{
+
+	let mut valid_moves : Vec<(uint, uint)>;
+
+	/* Can move right? */
+	if( zero_pos.val0() + 1 <= size - 1 ){
+		valid_moves.push((zero_pos.val0()+1,zero_pos.val1()));
 	}
+	/* Can move left? */
+	if( zero_pos.val0() - 1 >= 0 ){
+		valid_moves.push((zero_pos.val0()+1,zero_pos.val1()));
+	}
+	/* Can move up? */
+	if( zero_pos.val1() + 1 <= size - 1 ){
+		valid_moves.push((zero_pos.val0()+1,zero_pos.val1()));
+	}
+	/* Can move down? */
+	if( zero_pos.val1() - 1 >= 0 ){
+		valid_moves.push((zero_pos.val0()+1,zero_pos.val1()));
+	}
+
+	return valid_moves;
 }
---
+++ eight_puzzle.rs
@@ -0,0 +1,3 @@
+struct eight_puzzle{
+	board = [1,2,3,4,5,6,7,8,0]
+}
