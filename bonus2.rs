72288943ef67a1b85560c7dd625bb2c6da38688e
Nate Thompson <nthompson55@gmail.com>
2014-10-09 20:34:48 -0500

    valid moves works

--- bonus2.rs
+++ bonus2.rs
@@ -4,53 +4,64 @@ use std::vec;
 use std::rand;
 use std::io;
 
-static size: uint = 3;
+static size: int = 3;
 
 
 struct gameState{
 	board : Vec<Vec<uint>>,
 	moves_made : uint,
-	zero_pos : (uint, uint)
+	zero_pos : (int, int)
 }
 
 fn main() {
-	
+
 	let mut game : gameState;
-	game.zero_pos = (2u,2u);
-	game = generatePuzzle(game);
+	game.zero_pos = (2i,2i);
+	game = generatePuzzle();
 
 }
 
-fn generatePuzzle(game : gameState) -> gameState{
+fn generatePuzzle() -> gameState{
+	//new game state
+	let mut puzzle = gameState{
+		board: vec![vec![1,2,3], vec![4,5,6], vec![7,8,0]],
+		zero_pos: (1i, 1i),
+		moves_made: 0u
+	};
 
-	let mut board : Vec<Vec<uint>> = vec![vec![1,2,3], vec![4,5,6], vec![7,8,0]];
+
+	//find random number of moves to make
 	let mut num_rand_moves = (rand::random::<uint>() % 500u) + 1u;
 
-	validMove(game.zero_pos);
+	//scramble the board
+	//for x in range(0i, 10i){
+		//get valid moves
+		println!("Valid moves = {}",moveableTiles(puzzle.zero_pos));
+//	}
 
-	return board;
+	return puzzle;
 }
 
 /* If there are no valid moves, -1 is returned */
-fn validMove( zero_pos : (uint, uint) ) -> Vec<(uint, uint)>{
+fn moveableTiles( zero_pos : (int, int) ) -> Vec<(uint, uint)>{
 
-	let mut valid_moves : Vec<(uint, uint)>;
+	let mut valid_moves : Vec<(uint, uint)> = Vec::new();
 
 	/* Can move right? */
-	if( zero_pos.val0() + 1 <= size - 1 ){
-		valid_moves.push((zero_pos.val0()+1,zero_pos.val1()));
+	if( zero_pos.val0() + 1 <= size - 1  ){
+		valid_moves.push(((zero_pos.val0()+1) as uint,(zero_pos.val1()) as uint));
 	}
 	/* Can move left? */
 	if( zero_pos.val0() - 1 >= 0 ){
-		valid_moves.push((zero_pos.val0()+1,zero_pos.val1()));
+		valid_moves.push(((zero_pos.val0()-1) as uint,(zero_pos.val1()) as uint));
 	}
 	/* Can move up? */
 	if( zero_pos.val1() + 1 <= size - 1 ){
-		valid_moves.push((zero_pos.val0()+1,zero_pos.val1()));
+		valid_moves.push(((zero_pos.val0()) as uint,(zero_pos.val1()+1) as uint));
 	}
 	/* Can move down? */
 	if( zero_pos.val1() - 1 >= 0 ){
-		valid_moves.push((zero_pos.val0()+1,zero_pos.val1()));
+		valid_moves.push(((zero_pos.val0()) as uint,(zero_pos.val1()-1) as uint));
 	}
 
 	return valid_moves;
